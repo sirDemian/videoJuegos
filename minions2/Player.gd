87a48569512f6d42811c929ad22e35c441966423
sirDemian <sirdemian@gmail.com>
2018-10-07 21:16:02 -0300

    sabado de cerveza

--- minions2/Player.gd
+++ minions2/Player.gd
@@ -1,16 +1,18 @@
 extends KinematicBody2D
 
 export (float) var GRAVITY = 20
-export (float) var MAX_SPEED = 300
+export (float) var width_jump = 3
 export (float) var JUMP_HEIGTH = 2
 export (float) var ACCELERATION = 50
 
 const UP = Vector2(0,-1)
 var motion = Vector2()
 var real_jump = -425 * JUMP_HEIGTH
+var MAX_SPEED = width_jump * 107
+var currentState = "Idle"
 
 func _physics_process(delta):
-	var hero = get_node("PlayerAnimatedSprite")
+	var hero = $anim #get_node("anim")
 	var friction = false
 	motion.y += GRAVITY
 	
@@ -21,28 +23,33 @@ func _physics_process(delta):
 		motion.x = max(-MAX_SPEED , motion.x - ACCELERATION)
 		run_left(hero)
 	else:
-		hero.play("Idle")
+		setCurrentState("Idle")
 		friction = true
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
-			motion.y = real_jump	
+			motion.y = real_jump
 		if friction == true:
-			motion.x = lerp(motion.x , 0 , 0.3)
+			motion.x = lerp(motion.x , 0 , 0.15)
 			#inercia (desde, hasta, porcentaje de rozamiento)
 	else:
 		if friction == true:
-			motion.x = lerp(motion.x , 0 , 0.1)
+			motion.x = lerp(motion.x , 0 , 0.01)
 		if motion.y < 0:
-			hero.play("Jump")
+			setCurrentState("Jump")
 		else:
-			hero.play("Fall")
+			setCurrentState("Hurt")
 		
 	motion = move_and_slide(motion, UP)
 
 func run_rigth( a_sprite):
-	a_sprite.flip_h = false
-	a_sprite.play("Run")
+	#a_sprite.flip_h = false
+	setCurrentState("Run")
+		
+func setCurrentState(newState):
+	if(newState != currentState):
+		currentState = newState
+		$anim.play(currentState)
 	
 func run_left(a_sprite):
-	a_sprite.flip_h = true
-	a_sprite.play("Run")
+	#a_sprite.flip_h = true
+	setCurrentState("Run")
